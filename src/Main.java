033487d504863ff59b0e21866554a3f48a5fe5b4
620255 <620255@PC268735.cts.com>
2017-03-21 12:40:15 +0530

    example

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		String name;
+		int number;
+		float avg;
+	}
+
+}
